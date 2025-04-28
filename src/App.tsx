81d05a684f2898585269f70af613a77a85e56261
anakaibrahm <anakaibrahim16@gmail.com>
2025-04-28 07:39:52 +0700

    progress: 28/4/25

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import MatchSchedule from "./Pages/MatchSchedule/MatchSchedulePage";
 import OrderForm from "./Pages/OrderForm/OrderFormPage";
 import "./Styles/Global.css";
+import PaymentPage from "./Pages/Payment/PaymentPage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MatchSchedule />} />
+        <Route path="/" element={<PaymentPage />} />
         <Route path="/ticket-order/:MatchId" element={<OrderForm />} />
       </Routes>
     </Router>
---
+++ src/Pages/Payment/PaymentPage.tsx
@@ -0,0 +1,62 @@
+import { useRef } from "react";
+import styles from "./PaymentPage.module.css"; // Import CSS Module
+
+const PaymentPage = () => {
+  const inputRefs = [useRef(null), useRef(null), useRef(null), useRef(null)];
+
+  const handleChange = (e, index) => {
+    const value = e.target.value.replace(/\D/g, ""); // Hanya ambil angka
+    if (value.length >= 4) {
+      inputRefs[index + 1].current.focus(); // Pindah ke input berikutnya
+    }
+    e.target.value = value.replace(/(\d{4})(?=\d)/g, "$1 "); // Format dengan spasi
+  };
+
+  return (
+    <main className={styles["page-container"]}>
+      <div className={styles["payment-container"]}>
+        <h3>Rincian Pembayaran</h3>
+        <p>Garuda Timur 1x</p>
+        <p>Total: $100</p>
+      </div>
+      <div className={styles.container}>
+        <form className={styles.form}>
+          <div className={styles.formGroup}>
+            <label htmlFor="cardNumber">Nomor Kartu:</label>
+            <div className={styles.cardInputContainer}>
+              {Array.from({ length: 4 }).map((_, index) => (
+                <input
+                  key={index}
+                  type="text"
+                  maxLength={4}
+                  ref={inputRefs[index]}
+                  onChange={(e) => handleChange(e, index)}
+                  className={styles.input}
+                  placeholder="0000"
+                />
+              ))}
+            </div>
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="expiryDate">Tanggal Kadaluarsa (MM/YY):</label>
+            <input
+              type="text"
+              id="expiryDate"
+              required
+              className={styles.input}
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="cvv">CVV:</label>
+            <input type="text" id="cvv" required className={styles.input} />
+          </div>
+          <button type="submit" className={styles.submitButton}>
+            Bayar Sekarang
+          </button>
+        </form>
+      </div>
+    </main>
+  );
+};
+
+export default PaymentPage;
